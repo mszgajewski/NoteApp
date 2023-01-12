45301c37ebc2ce5375dc5930fa8f9bdd52d7a0b9
mszgajewski <mszgajewski@gmail.com>
2023-01-12 20:40:00 +0100

    Initial commit

---
+++ app/src/main/java/com/mszgajewski/noteapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.mszgajewski.noteapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Sprawdzanie uzytkownika");
+        progressDialog.setMessage("w toku");
+        progressDialog.show();
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() == null){
+            firebaseAuth.signInAnonymously()
+                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+
+                        }
+                    });
+        }
+    }
+}
